70cf94be9a57084c91f09989eea8fb487f173fa7
Michael M Slusarz <slusarz@users.noreply.github.com>
2024-06-17 22:30:34 -0600

    lua: Refactor Lua documentation pages
    Since Lua is important feature going forward, it is worthwhile to spend
    some time trying to improve the documentation.
    
    This may not be perfect, but it is better than it was.
    
    Main change is to move all data for constants, variables, and function
    processing to data files so that we can easily play with output in
    the future by mucking with the template.
    
    Additionally, for functions, try to display argument/parameter information
    more similar to existin settings display, for continuity purposes.
    
    Since there is so much information, break it up to multiple pages.

--- components/LuaFunctionComponent.vue
+++ components/LuaFunctionComponent.vue
@@ -8,39 +8,100 @@ import { data } from '../lib/data/lua.data.js'
 const props = defineProps(['level', 'tag'])
 const level = (props.level ? Number(props.level) : 2) + 1
 
-const d = Object.fromEntries(Object.entries(data).filter(([k, v]) =>
+const d = Object.fromEntries(Object.entries(data.functions).filter(([k, v]) =>
 	/* Filter entries (by tag). */
-	!props.tag ||
-	(v.tags && v.tags.includes(props.tag))
+	!props.tag || (v.tags === props.tag)
 ))
+
+function buildFunctionSignature(k, v) {
+	let out = k + '('
+	const args = []
+	const params = []
+	const opt_args = []
+
+	if (v.args) {
+		for (const [k2, v2] of Object.entries(v.args)) {
+			if (v2.hash_arg) {
+				params.push(k2 + '=' + v2.type)
+			} else if (v2.optional) {
+				opt_args.push(k2)
+			} else {
+				args.push(k2)
+				if (v2.multi) {
+					opt_args.push(k2)
+					opt_args.push('...')
+				}
+			}
+		}
+	}
+	return out + args.join(', ') +
+		(params.length
+			? '{ ' + params.join(', ') + ' }'
+			: '') +
+		(opt_args.length
+			? (args.length ? '[, ' : '[') + opt_args.join(', ') + ']'
+			: '') +
+		')'
+}
 </script>
 
 <template>
  <template v-for="(v, k) in d">
+  <div class="lua-function-separator" />
+
   <component :is="'h' + level" :id="k" tabindex="-1">
-   <code>{{ k }}</code>
+   <code>{{ k }}()</code>
    <a class="header-anchor" :href="'#' + k"></a>
   </component>
 
-  <dl v-if="v.args">
-   <dt><strong>Arguments:</strong></dt>
-   <dd>
-    <ul>
-     <li v-for="[k2,v2] in Object.entries(v.args)">
-      <code>{{ k2 }}</code>
-      (<em>{{ v2.type }}</em>):
-      <span v-html="v2.text" />
-      <span v-if="v2.default"> (Default: <code>{{ v2.default }}</code>)</span>
-     </li>
-    </ul>
-   </dd>
-  </dl>
+  <table v-if="v.args || v.return">
+   <tbody>
+    <tr>
+     <th style="text-align:right;">Signature</th>
+     <td>
+      <code>{{ buildFunctionSignature(k, v) }}</code>
+     </td>
+    </tr>
+    <tr v-if="v.args">
+     <th style="text-align:right;">Arguments</th>
+     <td>
+      <table>
+       <thead>
+        <tr>
+         <th>Name</th>
+         <th>Type</th>
+         <th>Description</th>
+        </tr>
+       </thead>
+       <tbody>
+        <tr v-for="[k2,v2] in Object.entries(v.args)">
+         <td><code>{{ k2 }}</code></td>
+         <td><em>{{ v2.type }}</em></td>
+         <td>
+          <span v-html="v2.text" />
+          <span v-if="v2.default">(Default: <code>{{ v2.default }}</code>)</span>
+         </td>
+        </tr>
+       </tbody>
+      </table>
+     </td>
+    </tr>
 
-  <dl v-if="v.return">
-   <dt><strong>Returns:</strong></dt>
-   <dd>{{ v.return }}</dd>
-  </dl>
+    <tr v-if="v.return">
+     <th style="text-align:right;">Returns</th>
+     <td>{{ v.return }}</td>
+    </tr>
+   </tbody>
+  </table>
 
   <span v-html="v.text"></span>
  </template>
 </template>
+
+<style scoped>
+.lua-function-separator {
+  border-top: 1px solid #e2e2e3;
+  margin: 15px auto;
+  width: 75%;
+}
+</style>
