531fc0e9ac725c43387bd25c6ce47c42708abecb
Aki Tuomi <aki.tuomi@open-xchange.com>
2024-07-03 19:20:02 +0300

    vitepress: Call download link packages

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -55,7 +55,7 @@ export default defineConfig({
 				link: 'https://www.dovecot.org/'
 			},
 			{
-				text: 'Repositories',
+				text: 'Packages',
 				link: 'https://repo.dovecot.org/'
 			},
 		],
