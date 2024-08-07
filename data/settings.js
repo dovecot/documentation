9860b3c0424f0db59aa983c297c3ab441b6b7805
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2024-08-07 16:00:17 +0200

    data: settings - Update description of push_notification and push_notification_driver settings

--- data/settings.js
+++ data/settings.js
@@ -2457,18 +2457,25 @@ This setting is unlikely to be ever needed.`
 
 	/* push-notification plugin */
 
-	push_notification_driver: {
+	push_notification: {
 		plugin: 'push-notification',
 		values: setting_types.STRING,
 		text: `
-The configuration value is the name of the driver, optionally
-followed by an \`:\` and driver-specific options (see
-[[link,push_notification]] for the list of drivers and options supported).
+The configuration value is a named filter for a specified driver, see
+[[link,push_notification]] for their names and their supported options.`
+	},
 
-It is possible to specify multiple push notification drivers by adding a
-sequential number to the \`push_notification_driver\` label, starting with
-the number \`2\`. There can be no numbering gaps for the labels; only the
-drivers that appear in sequential order will be processed.`
+	push_notification_driver: {
+		plugin: 'push-notification',
+		filter: 'push_notification',
+		dependencies: [
+			'push_notification',
+		],
+		values: setting_types.STRING,
+		text: `
+The name of the driver. This value determines the available options and the
+behavior. See [[link,push_notification]] for the list of supported drivers and
+options.`
 	},
 
 	/* quota-clone plugin */
