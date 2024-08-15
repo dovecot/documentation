df2fd2cac48c4331584830308776696e284d31ab
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2024-08-15 15:46:20 +0200

    util: generate_man - Make target dir recursively

--- util/generate_man.js
+++ util/generate_man.js
@@ -122,7 +122,7 @@ const processDovecotMd = () => {
 const main = async (component, outPath) => {
 	/* Create output directory, if it doesn't exist. */
 	if (!fs.existsSync(outPath)) {
-		await fs.promises.mkdir(outPath)
+		await fs.promises.mkdir(outPath, { recursive: true })
 	}
 
 	/* Generate list of man files. */
