8275e142f0266a0167580e369b567cb04fdb33ce
Aki Tuomi <aki.tuomi@open-xchange.com>
2025-08-29 12:51:21 +0300

    Add warning if this is not release version

--- .vitepress/theme/DovecotLayout.vue
+++ .vitepress/theme/DovecotLayout.vue
@@ -2,12 +2,26 @@
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 
-const { theme } = useData()
+const { site, theme } = useData()
 const { Layout } = DefaultTheme
+
+const re = new RegExp("^[/][0-9.]+[/]?$")
+const prod = re.test(site._value.base)
+const latest = site._value.themeConfig.dovecot.base_url + "/latest/"
+
 </script>
 
 <template>
  <Layout>
+  <template #doc-before v-if="!prod">
+   <div class="container custom-block warning large">
+    <p>
+     Note: This is pre-release documentation.<br/>
+     Please access <a :href="latest" target="_blank">{{ latest }}</a> for documentation on released versions.
+    </p>
+   </div>
+  </template>
+
   <template #doc-footer-before>
    <div class="edit-info">
     <div :class="'edit-rev edit-rev-' + theme.dovecot.gitrev.align">
