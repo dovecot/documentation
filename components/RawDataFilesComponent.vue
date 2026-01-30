def68fda566720f8e385a5d202317713d18c1ab4
Michael M Slusarz <slusarz@users.noreply.github.com>
2026-01-30 08:27:48 -0500

    components: Abstract raw file list HTML generation code

---
+++ components/RawDataFilesComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { data } from '../lib/data/datafiles.data.js'
+import { withBase } from 'vitepress'
+
+/* Properties for this component:
+ * - 'github' (boolean): If true, output GitHub link.
+ */
+const props = defineProps(['github'])
+const github = props.github
+</script>
+
+<template>
+ <table>
+  <thead>
+   <tr>
+    <th>File</th>
+    <th v-if="github">Source</th>
+   </tr>
+  </thead>
+  <tbody>
+   <tr v-for="file in data.files">
+    <td>
+     <code>
+      <a :href="withBase(file.download)" target="_blank" rel="noreferrer">{{ file.name }}</a>
+     </code>
+    </td>
+    <td v-if="github">
+     <a :href="file.github" target="_blank" rel="noreferrer">Source</a>
+    </td>
+   </tr>
+  </tbody>
+ </table>
+</template>
