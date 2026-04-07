4f3215d5c4df73e44420097b9c218bf52486d473
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-04-07 06:43:07 -0400

    spelling: also implicitly
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- data/settings.js
+++ data/settings.js
@@ -1586,7 +1586,7 @@ imapsieve_url = sieve://sieve.example.com
 		values: setting_types.BOOLEAN,
 		default: 'no',
 		text: `
-This setting determines whether de IMAPSieve plugin implicitly also expunges
+This setting determines whether de IMAPSieve plugin also implicitly expunges
 messages that were discarded by the executed Sieve script sequence; i.e., Sieve
 yielded no (implicit) keep.`
 	},
