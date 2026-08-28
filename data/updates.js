54609a04bfe192d47669b932ec244cc6dbf2c9f9
Timo Sirainen <timo.sirainen@open-xchange.com>
2026-08-28 12:56:36 +0000

    data/settings, upgrade: Document Sieve per-user rusage tracking changes

--- data/updates.js
+++ data/updates.js
@@ -83,6 +83,7 @@ export const updates = {
 	settings_syntax_named_filters_added: '2.4.0',
 	sieve_ext_imapflags: '2.4.0',
 	sieve_ext_notify: '2.4.0',
+	sieve_rusage_restore_changed: '2.4.5',
 	sieve_vnd_duplicate: '2.4.0',
 	sqlite_filename: '2.4.0',
 	setting_sqlite_synchronous_added: '2.4.3',
