e3cdff4e8ea66fc25c5b903822c8710bb73e730d
Timo Sirainen <timo.sirainen@open-xchange.com>
2026-09-17 21:46:00 +0000

    lua: Fix dict transaction set_timestamp() argument names

--- data/lua.js
+++ data/lua.js
@@ -703,12 +703,12 @@ end`
 	{
 		name: 'set_timestamp',
 		args: {
-			seconds: {
+			tv_sec: {
 				hash_arg: true,
 				type: 'int',
 				text: `UNIX timestamp.`
 			},
-			nanoseconds: {
+			tv_nsec: {
 				hash_arg: true,
 				type: 'int',
 				text: `Nanoseconds part of the timestamp.`
@@ -716,7 +716,8 @@ end`
 		},
 		tags: [ 'dict.transaction' ],
 		text: `
-Set timestamp to the dict transaction.
+Set write timestamp for the entire dict transaction. This must be called
+before any changes are done to the transaction.
 
 This is currently used only with Cassandra.`
 	},
